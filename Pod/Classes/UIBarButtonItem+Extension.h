eea010e3020363382ea437bc7c846a04e1861b7e
Mqhong <617257112@qq.com>
2016-01-12 17:51:49 +0800

    修改

--- Pod/Classes/UIBarButtonItem+Extension.h
+++ Pod/Classes/UIBarButtonItem+Extension.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "UIView+Extension.h"
 @interface UIBarButtonItem (Extension)
 + (UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image highImage:(NSString *)highImage;
 @end
